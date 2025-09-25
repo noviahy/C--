0927a1425d4dd212db0d901778ae29b9b51e5fa0
noviahy <noviahyunji@gmail.com>
2025-09-25 12:35:00 +0900

    25/09/25 (완)

--- HomeWork/Algorithm/BaekJoon/num9251.cpp
+++ HomeWork/Algorithm/BaekJoon/num9251.cpp
@@ -5,51 +5,52 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <algorithm> // max 해더파일
 using namespace std;
 
 class LCS
 {
 private:
-    vector<vector<int>> dp;
-    string lcs1;
-    string lcs2;
-    int n;
-    int m;
+    vector<vector<int>> dp; // DP 테이블
+    string lcs1, lcs2;      // 비교할 문자열
+    int n;                  // lcs1의 길이
+    int m;                  // lsc2의 길이
 
 public:
+    // 생성자: 문자열 초기화 및 DP 테이블 준비
     LCS(string str1, string str2)
         : lcs1(str1), lcs2(str2)
     {
-        n = str1.length();
+        n = str1.length(); // 길이 함수
         m = str2.length();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        this->dp = vector<vector<int>>(n + 1, vector<int>(m + 1, 0)); // vector 초기화
     }
 
+    // LCS 계산
     void DoLCS()
     {
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            for (int j = 1; j < m; i++)
+            for (int j = 1; j <= m; j++)
             {
-                if (lcs1[i - 1] == lcs2[j - 1])
-                {
-                }
+                if (lcs1[i - 1] == lcs2[j - 1]) // 문자가 같으면
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
 
-                else
-                {
-                    if
-                }
+                else                                            // 문자가 다르면
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); // max함수 사용으로 더 큰 값 선택
             }
         }
     }
 
-    void PrintResult()
+    // 결과 출력
+    void PrintResult() 
     {
         cout << dp[n][m] << endl;
     }
 };
 
-string InputString()
+// 입력 문자열 받기
+string InputString() 
 {
     string str;
 
@@ -65,6 +66,8 @@ int main()
     str1 = InputString();
     str2 = InputString();
     LCS lsc(str1, str2);
+    lsc.DoLCS();
+    lsc.PrintResult();
 
     return 0;
 }
