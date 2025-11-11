1fc5dec173c50108f6e7b4d4ba5fe4b945bb939d
noviahy <noviahyunji@gmail.com>
2025-11-11 11:03:21 +0900

    25/11/11

--- BaekJoon/num14565.cpp
+++ BaekJoon/num14565.cpp
@@ -7,23 +7,23 @@
 
 using namespace std;
 
-class Invserse
+class Inverse
 {
 private:
-    int N, A;
+    long long N, A;
 
 public:
-    Invserse(int N, int A) : N(N), A(A) {}
+    Inverse(long long N, long long A) : N(N), A(A) {}
 
-    int findadd() // 덧셈역
+    long long findadd() // 덧셈역
     {
-        return (A - N) % N;
+        return ((-A % N) + N) % N;
     }
 
-    int findmul() // 곱셈역
+    long long findmul() // 곱셈역
     {
-        int x, y;
-        int g = extended_gcd(A, N, x, y);
+        long long x, y;
+        long long g = extended_gcd(A, N, x, y);
 
         // 곱셈역에서 역원이 존재하려면 gcd(A, N) == 1이어야 함
         if (g != 1)
@@ -34,20 +34,20 @@ public:
     }
 
     // 확장 유클리드 알고리즘
-    int extended_gcd(int e, int b, int &x, int &y)
+    long long extended_gcd(long long a, long long n, long long &x, long long &y)
     {
-        if (b == 0) // 나머지 0
+        if (n == 0) // 나머지 0
         {
             x = 1;
             y = 0;
-            return e;
+            return a;
         }
 
-        int x1, y1; // 결과 받을 임시 변수
-        int g = extended_gcd(b, e % b, x1, y1); // 재귀 호출
+        long long x1, y1;                             // 결과 받을 임시 변수
+        long long g = extended_gcd(n, a % n, x1, y1); // 재귀 호출
 
         x = y1;
-        y = x1 - y1 * (e / b);
+        y = x1 - (a / n) * y1;
 
         return g;
     }
@@ -55,10 +55,10 @@ public:
 
 int main()
 {
-    int N, A;
+    long long N, A;
     cin >> N >> A;
 
-    Invserse in(N, A); // 생성자
+    Inverse in(N, A); // 생성자
 
     cout << in.findadd() << " " << in.findmul(); // 덧셈역, 곱셈역 출력
     return 0;
